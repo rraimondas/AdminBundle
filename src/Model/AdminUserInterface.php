ca942c2bb3765f303fa1bcce9ec32c85359cffe6
Aivaras Gotovskis <aivaras.gotovskis@nfq.lt>
2018-03-12 13:56:49 +0200

    Upgrade to sylius 1.0

--- src/Model/AdminUserInterface.php
+++ src/Model/AdminUserInterface.php
@@ -8,35 +8,35 @@ use Sylius\Component\User\Model\UserInterface;
 
 interface AdminUserInterface extends UserInterface
 {
-    const DEFAULT_ADMIN_ROLE = 'ROLE_ADMINISTRATION_ACCESS';
+    public const DEFAULT_ADMIN_ROLE = 'ROLE_ADMINISTRATION_ACCESS';
 
     /**
      * @return string
      */
-    public function getFirstName(): string;
+    public function getFirstName(): ?string;
 
     /**
      * @param string $firstName
      */
-    public function setFirstName(string $firstName): void;
+    public function setFirstName(?string $firstName): void;
 
     /**
      * @return string
      */
-    public function getLastName():string;
+    public function getLastName(): ?string;
 
     /**
      * @param string $lastName
      */
-    public function setLastName(string $lastName): void;
+    public function setLastName(?string $lastName): void;
 
     /**
      * @return string
      */
-    public function getLocaleCode(): string;
+    public function getLocaleCode(): ?string;
     
     /**
      * @param string $code
      */
-    public function setLocaleCode(string $code): void;
+    public function setLocaleCode(?string $code): void;
 }
